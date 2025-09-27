0a61979fca1511925d164d241a9ce9d5fd4a690c
Vitalii Bondarenko <frosty84@gmail.com>
2025-09-27 21:55:15 +0200

    Added simple test.

--- src/main/java/com/ratepay/challenge/controller/OrderController.java
+++ src/main/java/com/ratepay/challenge/controller/OrderController.java
@@ -2,11 +2,14 @@ package com.ratepay.challenge.controller;
 
 import com.ratepay.challenge.dto.OrderDto;
 import com.ratepay.challenge.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/orders")
 public class OrderController {
 
     OrderService orderService;
@@ -15,6 +18,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @GetMapping
     public List<OrderDto> getOrders() {
         return orderService.getOrders();
     }
--- src/test/java/com/ratepay/challenge/controller/OrderControllerTest.java
+++ src/test/java/com/ratepay/challenge/controller/OrderControllerTest.java
@@ -1,14 +1,27 @@
 package com.ratepay.challenge.controller;
 
 import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(OrderController.class)
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class OrderControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnListOfOrders() throws Exception {
+        mockMvc.perform(get("/api/orders"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+    }
 
 }
