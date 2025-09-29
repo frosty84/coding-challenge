372b0671e78f2321909ec7e88ddc4f3a6b17a022
Vitalii Bondarenko <frosty84@gmail.com>
2025-09-29 19:30:06 +0200

    Cleaning up.

--- src/main/java/com/ratepay/challenge/repository/OrderRepository.java
+++ src/main/java/com/ratepay/challenge/repository/OrderRepository.java
@@ -3,7 +3,6 @@ package com.ratepay.challenge.repository;
 import com.ratepay.challenge.entity.Order;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
